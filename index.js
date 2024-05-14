e57aaf2890a4c3a780a9eb092a0348137fbaaee6
wian0105 <wian.sanjaya0801@gmail.com>
2024-05-14 19:11:29 +0700

    adjust headers for api request

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 9090;
 
 const Food = require('./Food.js')
 
+// const corsOptions = {
+//     origin: 'http://localhost:5173' 
+// };
+
+
+app.use(cors());
 app.use(express.json())
 
 app.listen(
@@ -14,6 +21,22 @@ app.listen(
 );
 
 app.get('/food',  (req, res) => {
-    res.setHeader('Access-Controll-Allow-Origin', '*');
+    // req.setHeader('Access-Controll-Allow-Origin', '*');
+    // res.json(Food);
+
+    // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+
+    // // Request methods you wish to allow
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // // Set to true if you need the website to include cookies in the requests sent
+    // // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
     res.json(Food);
+
 });

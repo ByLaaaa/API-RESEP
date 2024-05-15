2f1c93363077d6877d8a526a700437589f70b529
wian0105 <wian.sanjaya0801@gmail.com>
2024-05-15 09:22:07 +0700

    update index

--- index.js
+++ index.js
@@ -21,22 +21,6 @@ app.listen(
 );
 
 app.get('/food',  (req, res) => {
-    // req.setHeader('Access-Controll-Allow-Origin', '*');
-    // res.json(Food);
-
-    // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-
-    // // Request methods you wish to allow
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // // Set to true if you need the website to include cookies in the requests sent
-    // // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
-
     res.json(Food);
 
 });
